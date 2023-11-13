d6c4a8f51a0876b8009c25a99d30f941daf2a20f
Minjoo <m0522inju@gmail.com>
2023-11-13 18:47:46 +0900

    refactor: 사용하지 않는 메소드 제거

--- src/main/java/christmas/exception/EventException.java
+++ src/main/java/christmas/exception/EventException.java
@@ -4,12 +4,4 @@ public class EventException extends IllegalArgumentException {
     public EventException() {
         super();
     }
-    public EventException(ExceptionMessage exceptionMessage) {
-        super(exceptionMessage.getMessage());
-    }
-
-    public EventException(ExceptionMessage exceptionMessage, Throwable cause) {
-        super(exceptionMessage.getMessage(), cause);
-    }
-
 }
